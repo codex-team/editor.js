a70a9831ebc9f6fb0ca4bbb78c5df199c29c31eb
Menshikov Alexander <n0str@users.noreply.github.com>
2016-07-02 15:36:47 +0300

    Link plugin frontend (#63)
    * Implement link download by enter press
    
    * Change promise structure
    
    * Fix promises fetch
    
    * Server meta-getter
    
    * Change keyEnterCode to ENTER_KEY
    
    * Add url and url-text parsing
    
    * Attached loader
    
    * Loader gif
    
    * Remove a fish :D

--- server/index.php
+++ server/index.php
@@ -12,7 +12,6 @@ function file_get_contents_curl($url)
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
     curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36');
 
-
     $data = curl_exec($ch);
     curl_close($ch);
 
