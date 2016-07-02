d696d94b1bd29d5e1205d93bcdbb9ed5abc6a9ed
Alexander Menshikov <ntpcp@yandex.ru>
2016-07-02 14:34:06 +0300

    Add timeouts

--- server/index.php
+++ server/index.php
@@ -8,6 +8,8 @@ function file_get_contents_curl($url)
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 8);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
     $data = curl_exec($ch);
     curl_close($ch);
