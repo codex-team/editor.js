f5b9bb7fe6b03160f3ae20d736424a696b8914ae
George Berezhnoy <gohabereg@gmail.com>
2017-01-23 22:13:21 +0300

    Small fix

--- codex-editor.js
+++ codex-editor.js
@@ -2972,7 +2972,7 @@ var codex =
 	                /**
 	                 * UNREPLACEBLE_TOOLS this types of tools are forbidden to replace even they are empty
 	                 */
-	                var UNREPLACEBLE_TOOLS = ['image', 'link', 'list', 'instagram', 'twitter', 'video'],
+	                var UNREPLACEBLE_TOOLS = ['image', 'link', 'list', 'instagram', 'twitter', 'embed'],
 	                    tool = codex.tools[codex.toolbar.current],
 	                    workingNode = codex.content.currentNode,
 	                    currentInputIndex = codex.caret.inputIndex,
