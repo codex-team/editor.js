3fb3b5f21ce37a3e25e122a8f220abd6909ac727
khaydarov <murod.haydarov@inbox.ru>
2017-01-26 01:10:00 +0300

    new paragraph module

---
+++ plugins/paragraph/paragraph.js
@@ -0,0 +1,104 @@
+/**
+* Paragraph Plugin
+* Creates DIV tag and adds content to this tag
+*/
+
+var paragraph = (function(paragraph) {
+
+    /**
+     * @protected
+     *
+     * Make initial header block
+     * @param {object} JSON with block data
+     * @return {Element} element to append
+     */
+    var _make = function (data) {
+
+        var tag = document.createElement('DIV');
+
+        tag.classList.add('ce-paragraph');
+
+        if (data && data.text) {
+            tag.innerHTML = data.text;
+        }
+
+        tag.contentEditable = true;
+
+        /**
+         * @uses Paste tool callback.
+         * Function analyzes pasted data
+         * If pasted URL from instagram, twitter or Image
+         * it renders via Social widgets content or uploads image and uses Image tool to render
+         */
+        tag.addEventListener('paste', codex.tools.paste.callbacks, false);
+
+        return tag;
+
+    };
+
+    /**
+     * @protected
+     *
+     * Handles input data for save
+     * @param data
+     * @private
+     */
+    var _prepareDataForSave = function(data) {
+
+    };
+
+    /**
+     * @public
+     *
+     * Plugins should have prepare method
+     * @param config
+     */
+    paragraph.prepare = function(config) {
+
+    };
+
+    /**
+     * @public
+     *
+     * Method to render HTML block from JSON
+     */
+    paragraph.render = function (data) {
+
+        return _make(data);
+
+    };
+
+    /**
+     * @public
+     *
+     * Check output data for validity.
+     * Should be defined by developer
+     */
+    paragraph.validate = function(output) {
+
+        if (output.text == '')
+            return;
+
+        return output;
+    };
+
+    /**
+     * @public
+     *
+     * Method to extract JSON data from HTML block
+     */
+    paragraph.save = function (blockContent){
+
+        var data = {
+            "text": blockContent.innerHTML,
+            "format": "html",
+            "introText": '<<same>>'
+        };
+
+        return data;
+
+    };
+
+    return paragraph;
+
+})({});
