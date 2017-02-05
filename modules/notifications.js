f732dfc848e5446529294cf084767eedd8eceaf4
George Berezhnoy <gohabereg@gmail.com>
2017-02-05 22:06:17 +0300

    Notifications improved

--- modules/notifications.js
+++ modules/notifications.js
@@ -8,42 +8,158 @@ let editor = codex.editor;
 
 module.exports = (function (notifications) {
 
+    var queue = [];
+
+    var addToQueue = function (closeNotification) {
+
+        queue.push(closeNotification);
+
+        if (queue.length > 5) {
+
+            var closeFirst = queue.shift();
+
+            closeFirst();
+
+        }
+
+    };
+
     /**
      * Error notificator. Shows block with message
      * @protected
      */
     notifications.errorThrown = function (errorMsg, event) {
 
-        editor.notifications.send('This action is not available currently', event.type, false);
+        editor.notifications.notification({message: 'This action is not available currently', type: event.type});
 
     };
 
     /**
-     * Appends notification with different types
-     * @param message {string} - Error or alert message
-     * @param type {string} - Type of message notification. Ex: Error, Warning, Danger ...
-     * @param append {boolean} - can be True or False when notification should be inserted after
+     *
+     * Appends notification
+     *
+     *  settings = {
+     *      type        - notification type (reserved types: alert, confirm, prompt). Just add class 'ce-notification-'+type
+     *      message     - notification message
+     *      okMsg       - confirm button text (default - 'Ok')
+     *      cancelBtn   - cancel button text (default - 'Cancel'). Only for confirm and prompt types
+     *      confirm     - function-handler for ok button click
+     *      cancel      - function-handler for cancel button click. Only for confirm and prompt types
+     *      time        - time (in seconds) after which notification will close (default - 10s)
+     *  }
+     *
+     * @param settings
      */
-    notifications.send = function (message, type, append) {
+    notifications.notification = function (constructorSettings) {
 
-        var notification = editor.draw.block('div');
+        var notification = null,
+            cancel       = null,
+            inputField   = null;
 
-        notification.textContent = message;
-        notification.classList.add('ce_notification-item', 'ce_notification-' + type, 'flipInX');
+        var create = function (settings) {
 
-        if (!append) {
+            if (!(settings && settings.message)) {
 
-            editor.nodes.notifications.innerHTML = '';
+                editor.core.log('Can\'t create notification. Message is missed');
+                return;
 
-        }
+            }
 
-        editor.nodes.notifications.appendChild(notification);
+            settings.type = settings.type || 'alert';
+            settings.time = settings.time*1000 || 10000;
 
-        window.setTimeout(function () {
+            var wrapper = editor.draw.node('DIV', 'ce-notification'),
+                message = editor.draw.node('DIV', 'ce-notification__message'),
+                input = editor.draw.node('INPUT', 'ce-notification__input'),
+                okBtn = editor.draw.node('SPAN', 'ce-notification__ok-btn'),
+                cancelBtn = editor.draw.node('SPAN', 'ce-notification__cancel-btn');
+
+            message.textContent = settings.message;
+            okBtn.textContent = settings.okMsg || 'Ок';
+            cancelBtn.textContent = settings.cancelMsg || 'Отмена';
+
+            okBtn.addEventListener('click', settings.confirm);
+            cancelBtn.addEventListener('click', settings.cancel);
+
+            okBtn.addEventListener('click', close);
+            cancelBtn.addEventListener('click', close);
+
+
+            wrapper.appendChild(message);
+
+            if (settings.type == 'prompt') {
+
+                wrapper.appendChild(input);
+
+            }
+
+            wrapper.appendChild(okBtn);
+
+            if (settings.type == 'prompt' || settings.type == 'confirm') {
+
+                wrapper.appendChild(cancelBtn);
+
+            }
+
+            wrapper.classList.add('ce-notification-'+settings.type);
+            wrapper.dataset.type = settings.type;
+
+            notification = wrapper;
+            cancel       = settings.cancel;
+            inputField   = input;
+
+            window.setTimeout(close, settings.time);
+
+        };
+
+        var send = function () {
+
+            editor.nodes.notifications.appendChild(notification);
+            inputField.focus();
+
+            editor.nodes.notifications.classList.add('ce-notification__notification-appending');
+
+            window.setTimeout(function () {
+
+                editor.nodes.notifications.classList.remove('ce-notification__notification-appending');
+
+            }, 100);
+
+            addToQueue(close);
+
+        };
+
+        var close = function () {
+
+            if (cancel) {
+
+                cancel();
+
+            }
 
             notification.remove();
 
-        }, 3000);
+        };
+
+        if (constructorSettings) {
+
+            create(constructorSettings);
+            send();
+
+        }
+
+        return {
+            create: create,
+            send: send,
+            close: close
+        };
+
+    };
+
+    notifications.clear = function () {
+
+        editor.nodes.notifications.innerHTML = '';
+        queue = [];
 
     };
 
