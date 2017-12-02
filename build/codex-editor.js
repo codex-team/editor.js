7fd0c14bf50de6ddd4a802ef7a979ed7081e5e53
Peter Savchenko <specc.dev@gmail.com>
2017-12-02 04:09:52 +0300

    Update util.Dom

--- build/codex-editor.js
+++ build/codex-editor.js
@@ -71,10 +71,6 @@ var CodexEditor =
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -84,7 +80,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 /**
  * DOM manupulations helper
  */
-var Dom = function () {
+module.exports = function () {
     function Dom() {
         _classCallCheck(this, Dom);
     }
@@ -188,10 +184,6 @@ var Dom = function () {
 
     return Dom;
 }();
-
-Dom.displayName = "Dom";
-exports.default = Dom;
-;
 
 /***/ }),
 /* 1 */
