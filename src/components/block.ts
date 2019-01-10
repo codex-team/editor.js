4f37f529c6d6726525e34d2e71ebc81918f35cec
PolinaShneider <beatrixlinkoln@gmail.com>
2019-01-10 20:43:44 +0300

    Call plugin's validate method with await

--- src/components/block.ts
+++ src/components/block.ts
@@ -387,7 +387,7 @@ export default class Block {
     let isValid = true;
 
     if (this.tool.validate instanceof Function) {
-      isValid = this.tool.validate(data);
+      isValid = await this.tool.validate(data);
     }
 
     return isValid;
