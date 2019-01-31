0043b1114629a5ba46e88af5098f60d19701639a
georgyb <yegor.berezhnoi@3d4medical.com>
2019-01-31 20:04:48 +0300

    Caret API

--- src/components/modules/api/caret.ts
+++ src/components/modules/api/caret.ts
@@ -1,6 +1,5 @@
 import Module from '../../__module';
 import {Caret} from '../../../../types/api';
-import {ModuleConfig} from '../../../types-internal/module-config';
 
 /**
  * @class CaretAPI
@@ -12,6 +11,67 @@ export default class CaretAPI extends Module {
    * @return {Caret}
    */
   get methods(): Caret {
-    return {};
+    return {
+      setToFirstBlock: this.setToFirstBlock,
+      setToLastBlock: this.setToLastBlock,
+      setToPreviousBlock: this.setToPreviousBlock,
+      setToNextBlock: this.setToNextBlock,
+      setToBlock: this.setToBlock,
+    };
+  }
+
+  /**
+   * Sets caret to the first Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  private setToFirstBlock = (position: string = this.Editor.Caret.positions.DEFAULT, offset: number = 0): void => {
+    this.Editor.Caret.setToBlock(this.Editor.BlockManager.firstBlock, position, offset);
+  }
+
+  /**
+   * Sets caret to the last Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  private setToLastBlock = (position: string = this.Editor.Caret.positions.DEFAULT, offset: number = 0): void => {
+    this.Editor.Caret.setToBlock(this.Editor.BlockManager.lastBlock, position, offset);
+  }
+
+  /**
+   * Sets caret to the previous Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  private setToPreviousBlock = (position: string = this.Editor.Caret.positions.DEFAULT, offset: number = 0): void => {
+    this.Editor.Caret.setToBlock(this.Editor.BlockManager.previousBlock, position, offset);
+  }
+
+  /**
+   * Sets caret to the next Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  private setToNextBlock = (position: string = this.Editor.Caret.positions.DEFAULT, offset: number = 0): void => {
+    this.Editor.Caret.setToBlock(this.Editor.BlockManager.nextBlock, position, offset);
+  }
+
+  /**
+   * Sets caret to the Block by passed index
+   *
+   * @param {number} index - index of Block where to set caret
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  private setToBlock = (
+    index: number,
+    position: string = this.Editor.Caret.positions.DEFAULT,
+    offset: number = 0,
+  ): void  => {
+    this.Editor.Caret.setToBlock(this.Editor.BlockManager.blocks[index], position, offset);
   }
 }
--- types/api/caret.d.ts
+++ types/api/caret.d.ts
@@ -1,6 +1,46 @@
 /**
  * Describes Editor`s caret API
- *
- * TODO
  */
-export interface Caret {}
+export interface Caret {
+
+  /**
+   * Sets caret to the first Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  setToFirstBlock(position?: 'end'|'start'|'default', offset?: number): void;
+
+  /**
+   * Sets caret to the last Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  setToLastBlock(position?: 'end'|'start'|'default', offset?: number): void;
+
+  /**
+   * Sets caret to the previous Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  setToPreviousBlock(position?: 'end'|'start'|'default', offset?: number): void;
+
+  /**
+   * Sets caret to the next Block
+   *
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  setToNextBlock(position?: 'end'|'start'|'default', offset?: number): void;
+
+  /**
+   * Sets caret to the Block by passed index
+   *
+   * @param {number} index - index of Block where to set caret
+   * @param {string} position - position where to set caret
+   * @param {number} offset - caret offset
+   */
+  setToBlock(index: number, position?: 'end'|'start'|'default', offset?: number): void;
+}
