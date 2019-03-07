afa8dc00d67910795bef85670d8e904add742e83
gohabereg <gohabereg@gmail.com>
2019-03-07 20:49:03 +0300

    Fix issue with pasting html to not initial tools

--- types/configs/paste-config.d.ts
+++ types/configs/paste-config.d.ts
@@ -11,14 +11,6 @@ export interface PasteConfig {
   tags?: string[];
 
   /**
-   * Handler to process pasted HTML tag
-   *
-   * @param {HTMLElement} element
-   * @return {BlockToolData}
-   */
-  handler?: (element: HTMLElement) => BlockToolData;
-
-  /**
    * Object of string patterns Tool can substitute.
    * Key is your internal key and value is RegExp
    *
@@ -27,24 +19,7 @@ export interface PasteConfig {
   patterns?: {[key: string]: RegExp};
 
   /**
-   * Handler to process pasted patterns
-   *
-   * @param {string} text
-   * @param {string} key
-   * @return {BlockToolData}
-   */
-  patternHandler?: (text: string, key: string) => BlockToolData;
-
-  /**
    * Object with arrays of extensions and MIME types Tool can substitute
    */
   files?: {extensions?: string[], mimeTypes?: string[]};
-
-  /**
-   * Handler to process pasted files
-   *
-   * @param {File} file
-   * @return {BlockToolData}
-   */
-  fileHandler?: (file: File) => BlockToolData;
 }
