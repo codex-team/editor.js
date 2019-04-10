5b20b27444ab678948feaf928578010a9c7b1809
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2019-04-10 16:41:00 +0530

    renamed inline -> inline-toolbar

--- src/types-internal/editor-modules.d.ts
+++ src/types-internal/editor-modules.d.ts
@@ -30,7 +30,7 @@ import SaverAPI from '../components/modules/api/saver';
 import Saver from '../components/modules/saver';
 import BlockSelection from '../components/modules/blockSelection';
 import RectangleSelection from '../components/modules/RectangleSelection';
-import InlineToolbarAPI from '../components/modules/api/inline';
+import InlineToolbarAPI from '../components/modules/api/inline-toolbar';
 
 export interface EditorModules {
   UI: UI;
