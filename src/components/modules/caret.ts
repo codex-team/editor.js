9098c68c759835f0c15de62b15c7c44d354833bc
Yegor Berizhnoi <yegor.berezhnoi@3d4medical.com>
2019-11-30 23:26:13 +0300

    Remove console.trace

--- src/components/modules/caret.ts
+++ src/components/modules/caret.ts
@@ -221,7 +221,6 @@ export default class Caret extends Module {
    * @param {Number} offset - caret offset regarding to the text node
    */
   public setToBlock(block: Block, position: string = this.positions.DEFAULT, offset: number = 0): void {
-    console.trace();
     const {BlockManager} = this.Editor;
     let element;
 
