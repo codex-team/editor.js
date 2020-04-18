f1721071495078fe42dd27b3c939ca969251299f
athul7744 <athul7744@outlook.com>
2020-04-18 23:33:20 +0530

    Making index value optional for delete operation

--- src/components/modules/api/blocks.ts
+++ src/components/modules/api/blocks.ts
@@ -19,7 +19,7 @@ export default class BlocksAPI extends Module {
       clear: () => this.clear(),
       render: (data: OutputData) => this.render(data),
       renderFromHTML: (data: string) => this.renderFromHTML(data),
-      delete: (index: number) => this.delete(index),
+      delete: (index?: number) => this.delete(index),
       swap: (fromIndex: number, toIndex: number) => this.swap(fromIndex, toIndex),
       move: (toIndex: number, fromIndex?: number) => this.move(toIndex, fromIndex),
       getBlockByIndex: (index: number) => this.getBlockByIndex(index),
