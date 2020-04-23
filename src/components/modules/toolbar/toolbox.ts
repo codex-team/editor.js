22f91957f3fc1c81d6784f8c26e6da9573279954
dummy <dummy>
2020-04-23 11:54:07 +0900

    Add hidden option to toolbox

--- src/components/modules/toolbar/toolbox.ts
+++ src/components/modules/toolbar/toolbox.ts
@@ -179,6 +179,10 @@ export default class Toolbox extends Module {
 
     const userToolboxSettings = this.Editor.Tools.getToolSettings(toolName)[userSettings.TOOLBOX] || {};
 
+    if (userToolboxSettings.hidden === undefined ? toolToolboxSettings.hidden : userToolboxSettings.hidden === true) {
+      return ;
+    }
+
     const button = $.make('li', [ this.CSS.toolboxButton ]);
 
     button.dataset.tool = toolName;
