05f09006aabb84fb06d7d89294811a6e4830ab8e
ranemihir <mihirrane171@gmail.com>
2020-09-03 19:26:14 +0530

    Fix lint errors

--- src/components/core.ts
+++ src/components/core.ts
@@ -226,7 +226,7 @@ export default class Core {
 
     /**
      * The default order of Inline tools. Will be applied if user will pass the `InlineToolbar: true` through the tool options.
-     * If it is false, the Inline Toolbar will be disabled for all the tools. 
+     * If it is false, the Inline Toolbar will be disabled for all the tools.
      */
     this.config.inlineToolbar = config.inlineToolbar || [];
   }
