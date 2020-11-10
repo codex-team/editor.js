f4c65d54d57cc31dda11c17aea3a5bdbc56b1f8b
ranemihir <mihirrane171@gmail.com>
2020-11-10 13:16:48 +0530

    Assert paragraph data in editor instance

--- cypress/integration/basic/initialization.spec.js
+++ cypress/integration/basic/initialization.spec.js
@@ -10,16 +10,44 @@ describe('Editor.js with default configuration', () => {
   it('create editor js instance on the page and check if it is visible', () => {
     cy.visit(Cypress.env('local_test_server_url'));
 
-    cy.window().then(window => {
-      const editor = new window.EditorJS({
-        holder: 'editorjs'
-      })
+    /**
+     * Initialize editor with some paragraph data imported form basic/paragraph_data.json fixture.
+     */
+    cy.fixture('basic/paragraph_data').then((paragraph) => {
+      cy.window().then(window => {
+        const editor = new window.EditorJS({
+          holder: 'editorjs',
+          data: {
+            blocks: [
+              {
+                type: 'paragraph',
+                data: {
+                  text: paragraph.data
+                }
+              }
+            ]
+          }
+        })
 
-      expect(editor).to.be.instanceOf(window.EditorJS)
+        /**
+         * Assert the created editor instance.
+         */
+        expect(editor).to.be.instanceOf(window.EditorJS)
+      })
     })
 
+    /**
+     * Assert if editor is created in the UI or not.
+     */
     cy.get('div#editorjs')
       .get('div.codex-editor')
       .should('be.visible');
+
+    /**
+     * Assert if the provided pragraph data is present in the editor instance or not.
+     */
+    cy.fixture('basic/paragraph_data').then((paragraph) => {
+      cy.contains('div.ce-paragraph', paragraph.data)
+    })
   })
 });
