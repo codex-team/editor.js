ca0bb6b7cdfc2d6d755997f8507df7b36718bf96
ranemihir <mihirrane171@gmail.com>
2020-11-10 12:00:18 +0530

    Create editor instance in the test

--- cypress/integration/basic/initialization.spec.js
+++ cypress/integration/basic/initialization.spec.js
@@ -1,18 +1,25 @@
-describe('Initialization Tests', () => {
-  it('check if editor js module is imported or not', () => {
+describe('Editor.js with default configuration', () => {
+  it('should have a global EditorJS function', () => {
     cy.visit(Cypress.env('local_test_server_url'));
 
     cy.window().then(window => {
       assert.isFunction(window.EditorJS, 'window.EditorJS is a function');
     });
-
   });
 
-  it('check initialization', () => {
+  it('create editor js instance on the page and check if it is visible', () => {
     cy.visit(Cypress.env('local_test_server_url'));
-    
+
+    cy.window().then(window => {
+      const editor = new window.EditorJS({
+        holder: 'editorjs'
+      })
+
+      expect(editor).to.be.instanceOf(window.EditorJS)
+    })
+
     cy.get('div#editorjs')
       .get('div.codex-editor')
       .should('be.visible');
-  });
+  })
 });
