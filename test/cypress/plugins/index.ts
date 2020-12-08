b78707fb78c34959678dae1c1b423264711f41b1
Peter Savchenko <specc.dev@gmail.com>
2020-12-08 17:14:47 +0300

    fix eslint

--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -2,4 +2,4 @@
  * This file contains connection of Cypres plugins
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export default function(on, config): void {}
+// export default function(on, config): void {}
