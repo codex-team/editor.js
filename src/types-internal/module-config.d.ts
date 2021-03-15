d945d4c5189f126b37feeed8c818ac8e0ef00912
Murod Khaydarov <murod.haydarov@gmail.com>
2021-03-15 21:01:10 +0300

    refactoring(modules): Events module is util now (#1582)
    * refactoring(modules): events module is util now
    
    * Update changelog
    
    * remove redundant line
    
    * refactor
    
    * Update editor-modules.d.ts
    
    Co-authored-by: Peter Savchenko <specc.dev@gmail.com>

--- src/types-internal/module-config.d.ts
+++ src/types-internal/module-config.d.ts
@@ -1,8 +1,10 @@
-import {EditorConfig} from '../../types/index';
+import { EditorConfig } from '../../types/index';
+import EventsDispatcher from '../components/utils/events';
 
 /**
  * Describes object passed to Editor modules constructor
  */
 export interface ModuleConfig {
   config: EditorConfig;
+  eventsDispatcher: EventsDispatcher;
 }
