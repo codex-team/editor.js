3907f2010294460d836fafe934b044686f5d2af6
Peter Savchenko <specc.dev@gmail.com>
2021-03-15 20:49:03 +0300

    Update editor-modules.d.ts

--- src/types-internal/editor-modules.d.ts
+++ src/types-internal/editor-modules.d.ts
@@ -4,7 +4,6 @@ import Toolbar from '../components/modules/toolbar/index';
 import InlineToolbar from '../components/modules/toolbar/inline';
 import Toolbox from '../components/modules/toolbar/toolbox';
 import BlockSettings from '../components/modules/toolbar/blockSettings';
-import Events from '../components/utils/events';
 import Shortcuts from '../components/modules/shortcuts';
 import Paste from '../components/modules/paste';
 import Notifier from '../components/modules/notifier';
