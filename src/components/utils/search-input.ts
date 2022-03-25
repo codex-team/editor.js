964ceba7cde12cc62228f139f2a3b0774e62fed4
Peter Savchenko <specc.dev@gmail.com>
2022-03-25 18:23:58 +0300

    popover mobile styles improved

--- src/components/utils/search-input.ts
+++ src/components/utils/search-input.ts
@@ -26,10 +26,12 @@ export default class SearchInput {
    */
   private static get CSS(): {
     input: string;
+    icon: string;
     wrapper: string;
     } {
     return {
       wrapper: 'cdx-search-field',
+      icon: 'cdx-search-field__icon',
       input: 'cdx-search-field__input',
     };
   }
@@ -80,13 +82,15 @@ export default class SearchInput {
    */
   private render(placeholder: string): void {
     this.wrapper = Dom.make('div', SearchInput.CSS.wrapper);
+    const iconWrapper = Dom.make('div', SearchInput.CSS.icon);
     const icon = $.svg('search', 16, 16);
 
     this.input = Dom.make('input', SearchInput.CSS.input, {
       placeholder,
     }) as HTMLInputElement;
 
-    this.wrapper.appendChild(icon);
+    iconWrapper.appendChild(icon);
+    this.wrapper.appendChild(iconWrapper);
     this.wrapper.appendChild(this.input);
 
     this.listeners.on(this.input, 'input', () => {
