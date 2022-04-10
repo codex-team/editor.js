99035cb1b0e7eda91842c6f15a452377ca595daa
ilyamore88 <moroz.ilya.olegovich@gmail.com>
2022-04-10 16:04:02 +0300

    remove initial state

--- src/components/store/index.ts
+++ src/components/store/index.ts
@@ -1,29 +1,6 @@
 import reducer from './reducer';
 import createStore from './createStore';
 
-const initialState = {
-  data: {
-    time: 1649592664578,
-    blocks: [
-      {
-        id: '3JPEqh8_Wc',
-        type: 'header',
-        data: {
-          text: 'Editor.js',
-          level: 2,
-        },
-      },
-      {
-        id: 'AsbMKCuatV',
-        type: 'paragraph',
-        data: {
-          text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
-        },
-      },
-    ],
-  },
-};
-
-const store = createStore(reducer, initialState);
+const store = createStore(reducer);
 
 export default store;
