8f156a87ea42fcf9f398719a2236e89accd85662
Peter Savchenko <specc.dev@gmail.com>
2022-04-25 18:28:58 +0300

    feat(ui): the toolbox became vertical (#2014)
    * the popover component, vertical toolbox
    
    * toolbox position improved
    
    * popover width improved
    
    * always show the plus button
    
    * search field added
    
    * search input in popover
    
    * trying to create mobile toolbox
    
    * feat(toolbox): popover adapted for mobile devices (#2004)
    
    * FIx mobile popover fixed positioning
    
    * Add mobile popover overlay
    
    * Hide mobile popover on scroll
    
    * Alter toolbox buttons hover
    
    * Fix closing popover on overlay click
    
    * Tests fix
    
    * Fix onchange test
    
    * restore focus after toolbox closing by ESC
    
    * don't move toolbar by block-hover on mobile
    
    Resolves #1972
    
    * popover mobile styles improved
    
    * Cleanup
    
    * Remove scroll event listener
    
    * Lock scroll on mobile
    
    * don't show shortcuts in mobile popover
    
    * Change data attr name
    
    * Remove unused styles
    
    * Remove unused listeners
    
    * disable hover on mobile popover
    
    * Scroll fix
    
    * Lint
    
    * Revert "Scroll fix"
    
    This reverts commit 82deae543eadd5c76b9466e7533bf3070d82ac4c.
    
    * Return back background color for active state of toolbox buttons
    
    Co-authored-by: Peter Savchenko <specc.dev@gmail.com>
    
    * Vertical toolbox fixes (#2017)
    
    * Replace visibility property with display for hiding popover
    
    * Disable arrow right and left keys for popover
    
    * Revert "Replace visibility property with display for hiding popover"
    
    This reverts commit af521cf6f29fb06b71a0e2e8ec88d6a757f9144f.
    
    * Hide popover via setting max-height to 0 to fix animation in safari
    
    * Remove redundant condition
    
    * Extend element interface to avoid ts errors
    
    * Do not subscribe to block hovered if mobile
    
    * Add unsubscribing from overlay click event
    
    * Rename isMobile to isMobileScreen
    
    * Cleanup
    
    * fix: popover opening direction (#2022)
    
    * Change popover opening direction based on available space below it
    
    * Update check
    
    * Use cacheable decorator
    
    * Update src/components/flipper.ts
    
    Co-authored-by: George Berezhnoy <gohabereg@users.noreply.github.com>
    
    * Fixes
    
    * Fix test
    
    * Clear search on popover hide
    
    * Fix popover width
    
    * Fix for tests
    
    * Update todos
    
    * Linter fixes
    
    * rm todo about beforeInsert
    
    because I have no idea what does it mean
    
    * i18n for search labels done
    
    * rm methods for hiding/showing of +
    
    * some code style update
    
    * Update CHANGELOG.md
    
    * make the list items a little bit compact
    
    * fix z-index issue caused by block-appearing animation
    
    also, improve popover padding for two reasons:
    
    - make the popover more consistent with the Table tool popover (in future, it can be done with the same api method)
    - make popover looks better
    
    Co-authored-by: Tanya Fomina <fomina.tatianaaa@yandex.ru>
    Co-authored-by: George Berezhnoy <gohabereg@users.noreply.github.com>

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -762,3 +762,10 @@ export function cacheable<Target, Value, Arguments extends unknown[] = unknown[]
 
   return descriptor;
 };
+
+/**
+ * True if screen has mobile size
+ */
+export function isMobileScreen(): boolean {
+  return window.matchMedia('(max-width: 650px)').matches;
+}
