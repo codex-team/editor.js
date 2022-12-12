b7977c92fbeb5e92238a440e126faa7d76032263
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2022-12-12 22:03:42 +0200

    Lint

--- src/components/utils/resolve-aliases.ts
+++ src/components/utils/resolve-aliases.ts
@@ -1,10 +1,9 @@
 /**
  * Resolves aliases in specified object according to passed aliases info
- * 
+ *
  * @example resolveAliases(obj, { label: 'title' })
  * here 'label' is alias for 'title'
- *
- * @param item - item with resolved aliases
+ * @param obj - object with aliases to be resolved
  * @param aliases - object with aliases info where key is an alias property name and value is an aliased property name
  */
 export function resolveAliases<ObjectType>(obj: ObjectType, aliases: { [alias: string]: string }): ObjectType {
