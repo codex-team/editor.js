8f9809b78c06c731a1a7bb6bfdafd4f53214299d
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2023-01-07 15:23:46 +0200

    Add new popover class

---
+++ src/components/utils/popover/index.ts
@@ -0,0 +1,187 @@
+import './styles.css'; // not working
+
+import { PopoverItemNode, PopoverItemParams } from './popover-item';
+import Dom from '../../dom';
+import { cacheable } from '../../utils';
+
+interface PopoverParams {
+  items: PopoverItemParams[];
+  scopeElement?: HTMLElement;
+}
+
+/**
+ * Class responsible for rendering popover and handling its behaviour
+ */
+export default class Popover {
+  /** List of popover items */
+  private items: PopoverItemNode[];
+
+  /** True if popover is open */
+  private isOpen = false;
+
+  /**
+   * Element of the page that creates 'scope' of the popover.
+   * If possible, popover will not cross specified element's borders when opening.
+   */
+  private scopeElement: HTMLElement = document.body;
+
+  /**
+   * Popover CSS classes
+   */
+  private static get CSS(): {
+    popover: string;
+    popoverOpenTop: string;
+    popoverClosed: string
+    } {
+    return {
+      popover: 'codex-popover',
+      popoverOpenTop: 'codex-popover--open-top',
+      popoverClosed: 'codex-popover--closed',
+    };
+  }
+
+  /**
+   * Refs to created HTML elements
+   */
+  private nodes: {
+    popover: HTMLElement | null
+  } = {
+      popover: null,
+    };
+
+  /**
+   * Constructs the instance
+   *
+   * @param params - popover construction params
+   */
+  constructor(params: PopoverParams) {
+    this.items = params.items.map(item => new PopoverItemNode(item));
+
+    if (params.scopeElement !== undefined) {
+      this.scopeElement = params.scopeElement;
+    }
+
+    this.make();
+  }
+
+  /**
+   * Returns HTML element correcponding to the popover
+   */
+  public getElement(): HTMLElement | null {
+    return this.nodes.popover;
+  }
+
+  /**
+   * Open popover
+   */
+  public show(): void {
+    if (!this.shouldOpenBottom) {
+      this.nodes.popover.style.setProperty('--popover-height', this.height + 'px');
+      this.nodes.popover.classList.add(Popover.CSS.popoverOpenTop);
+    }
+
+    this.nodes.popover.classList.remove(Popover.CSS.popoverClosed);
+    this.isOpen = true;
+  }
+
+  /**
+   * Closes popover
+   */
+  public hide(): void {
+    this.nodes.popover.classList.add(Popover.CSS.popoverClosed);
+    this.nodes.popover.classList.remove(Popover.CSS.popoverOpenTop);
+    this.isOpen = false;
+  }
+
+  /**
+   *
+   */
+  public destroy(): void {
+    //
+  }
+
+
+  /**
+   * Constructs HTML element corresponding to popover
+   */
+  private make(): void {
+    this.nodes.popover = Dom.make('div', [Popover.CSS.popover, Popover.CSS.popoverClosed]);
+
+    this.items.forEach(item => {
+      this.nodes.popover.appendChild(item.getElement());
+    });
+
+    this.nodes.popover.addEventListener('click', (event: PointerEvent) => {
+      const item = this.getTargetItem(event);
+
+      this.handleClick(item, event);
+    });
+  }
+
+  /**
+   * Retrieves popover item that is the target of the specified event
+   *
+   * @param event - event to retrieve popover item from
+   */
+  private getTargetItem(event: PointerEvent): PopoverItemNode | undefined {
+    return this.items.find(el => event.composedPath().includes(el.getElement()));
+  }
+
+  /**
+   * Handles item clicks
+   *
+   * @param item - item to handle click of
+   * @param event - click event
+   */
+  private handleClick(item: PopoverItemNode, event: PointerEvent): void {
+    if (item === undefined || item.isDisabled) {
+      return;
+    }
+
+    item.onActivate(event);
+
+    if (item.toggle === true) {
+      item.toggleActive();
+    }
+  }
+
+  /**
+   * Helps to calculate height of popover while it is not displayed on screen.
+   * Renders invisible clone of popover to get actual height.
+   */
+  @cacheable
+  private get height(): number {
+    let height = 0;
+
+    if (this.nodes.popover === null) {
+      return height;
+    }
+
+    const popoverClone = this.nodes.popover.cloneNode(true) as HTMLElement;
+
+    popoverClone.style.visibility = 'hidden';
+    popoverClone.style.position = 'absolute';
+    popoverClone.style.top = '-1000px';
+    popoverClone.classList.remove(Popover.CSS.popoverClosed);
+    document.body.appendChild(popoverClone);
+    height = popoverClone.offsetHeight;
+    popoverClone.remove();
+
+    return height;
+  }
+
+  /**
+   * Checks if popover should be opened bottom.
+   * It should happen when there is enough space below or not enough space above
+   */
+  private get shouldOpenBottom(): boolean {
+    const popoverRect = this.nodes.popover.getBoundingClientRect();
+    const scopeElementRect = this.scopeElement.getBoundingClientRect();
+    const popoverHeight = this.height;
+    const popoverPotentialBottomEdge = popoverRect.top + popoverHeight;
+    const popoverPotentialTopEdge = popoverRect.top - popoverHeight;
+    const bottomEdgeForComparison = Math.min(window.innerHeight, scopeElementRect.bottom);
+
+    return popoverPotentialTopEdge < scopeElementRect.top || popoverPotentialBottomEdge <= bottomEdgeForComparison;
+  }
+}
---
+++ src/components/utils/popover/popover-item/index.ts
@@ -0,0 +1,142 @@
+import Dom from '../../../dom';
+import { IconDotCircle } from '@codexteam/icons';
+
+import './styles.css'; // not working
+
+export interface PopoverItemParams {
+  /** Popover item icon svg */
+  icon?: string;
+
+  /** Popover item title */
+  title?: string;
+
+  /** Popover item secondary title */
+  secondaryLabel?: string;
+
+  /** True if popover item should be highlighted as active */
+  isActive?: boolean;
+
+  /** True if popover item should be marked as disabled */
+  isDisabled?: boolean;
+
+  /**  */
+  toggle?: boolean;
+
+  /** Popover item name. Used for testing purposes */
+  cyName?: string;
+
+  onActivate: (item: PopoverItemParams, event?: PointerEvent) => void;
+}
+
+
+/**
+ * Represents sigle popover item node
+ */
+export class PopoverItemNode {
+  /** True if item is disabled and hence not clickable */
+  public get isDisabled(): boolean {
+    return this.params.isDisabled;
+  }
+
+  /** */
+  public get toggle(): boolean {
+    return this.params.toggle;
+  }
+
+  /** Item root html element */
+  private element: HTMLElement;
+
+  /**  */
+  private params: PopoverItemParams;
+
+  /**
+   * Popover item CSS classes
+   */
+  private static get CSS(): {
+    container: string,
+    title: string,
+    secondaryTitle: string,
+    icon: string,
+    isActive: string,
+    isDisabled: string,
+    } {
+    return {
+      container: 'codex-popover-item',
+      title: 'codex-popover-item__title',
+      secondaryTitle: 'codex-popover-item__secondary-title',
+      icon: 'codex-popover-item__icon',
+      isActive: 'codex-popover-item--active',
+      isDisabled: 'codex-popover-item--disabled',
+    };
+  }
+
+  /**
+   * Constructs popover item instance
+   *
+   * @param params - popover item construction params
+   */
+  constructor(params: PopoverItemParams) {
+    this.params = params;
+    this.element = this.make(params);
+  }
+
+  /**
+   * Returns popover item root element
+   */
+  public getElement(): HTMLElement {
+    return this.element;
+  }
+
+  /**
+   * Called on popover item click
+   *
+   * @param event - click event
+   */
+  public onActivate(event: PointerEvent): void {
+    this.params.onActivate(this.params, event);
+  }
+
+  /**
+   * Toggles item active state
+   */
+  public toggleActive(): void {
+    this.element.classList.toggle(PopoverItemNode.CSS.isActive);
+  }
+
+  /**
+   * Constructs HTML element corresponding to popover item
+   *
+   * @param params - item construction params
+   */
+  private make(params: PopoverItemParams): HTMLElement {
+    const el = Dom.make('div', PopoverItemNode.CSS.container);
+
+    if (params.cyName) {
+      el.dataset.cyName = params.cyName;
+    }
+
+    el.appendChild(Dom.make('div', PopoverItemNode.CSS.icon, {
+      innerHTML: params.icon || IconDotCircle,
+    }));
+
+    el.appendChild(Dom.make('div', PopoverItemNode.CSS.title, {
+      innerHTML: params.title || '',
+    }));
+
+    if (params.secondaryLabel) {
+      el.appendChild(Dom.make('div', PopoverItemNode.CSS.secondaryTitle, {
+        textContent: params.secondaryLabel,
+      }));
+    }
+
+    if (params.isActive) {
+      el.classList.add(PopoverItemNode.CSS.isActive);
+    }
+
+    if (params.isDisabled) {
+      el.classList.add(PopoverItemNode.CSS.isDisabled);
+    }
+
+    return el;
+  }
+}
