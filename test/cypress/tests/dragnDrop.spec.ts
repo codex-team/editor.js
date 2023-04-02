728c1ca2162dab4ab1e2b5543087868445db4377
robonetphy <1998patelumang@gmail.com>
2023-04-02 12:14:11 +0530

    removed the lint

--- test/cypress/tests/dragnDrop.spec.ts
+++ test/cypress/tests/dragnDrop.spec.ts
@@ -12,6 +12,7 @@ describe('Drag and drop the block of Editor', function () {
     }).as('editorInstance');
 
     const numberOfBlocks = 3;
+
     for (let i = 0; i < numberOfBlocks; i++) {
       cy.get('[data-cy=editorjs]')
         .get('div.ce-block')
@@ -43,8 +44,9 @@ describe('Drag and drop the block of Editor', function () {
       cy.fixture(fileName, 'binary')
         .then(Cypress.Blob.binaryStringToBlob)
         .then((blob) => {
-          const file = new File([blob], fileName, { type: fileType });
+          const file = new File([ blob ], fileName, { type: fileType });
           const dataTransfer = new DataTransfer();
+
           // add the file to the DataTransfer object
           dataTransfer.items.add(file);
 
@@ -54,8 +56,7 @@ describe('Drag and drop the block of Editor', function () {
             .last()
             .trigger('dragenter')
             .trigger('dragover')
-            .trigger('drop', { dataTransfer })
-            .wait(1000);
+            .trigger('drop', { dataTransfer });
 
           cy.get('[data-cy=editorjs]')
             // In Edge test are performed slower, so we need to
@@ -67,11 +68,16 @@ describe('Drag and drop the block of Editor', function () {
   });
 
   it('should have block dragover style on the top of target block', function () {
-    cy.get('[data-cy=editorjs]').get('div.ce-block').first().click();
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .first()
+      .click();
 
     const dataTransfer = new DataTransfer();
 
-    cy.get('.ce-toolbar__settings-btn').trigger('dragstart', { dataTransfer });
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragstart', { dataTransfer });
 
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
@@ -96,6 +102,7 @@ describe('Drag and drop the block of Editor', function () {
           .then(($element) => {
             // check for dragover top style on target element.
             const classes = $element.attr('class').split(' ');
+
             expect(classes).to.include('ce-block--drop-target');
             expect(classes).to.include('ce-block--drop-target-top');
           });
@@ -103,11 +110,16 @@ describe('Drag and drop the block of Editor', function () {
   });
 
   it('should have block dragover style on the bottom of target block', function () {
-    cy.get('[data-cy=editorjs]').get('div.ce-block').first().click();
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .first()
+      .click();
 
     const dataTransfer = new DataTransfer();
 
-    cy.get('.ce-toolbar__settings-btn').trigger('dragstart', { dataTransfer });
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragstart', { dataTransfer });
 
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
@@ -132,6 +144,7 @@ describe('Drag and drop the block of Editor', function () {
           .then(($element) => {
             // check for dragover top style on target element.
             const classes = $element.attr('class').split(' ');
+
             expect(classes).to.include('ce-block--drop-target');
             expect(classes).to.include('ce-block--drop-target-bottom');
           });
@@ -139,108 +152,14 @@ describe('Drag and drop the block of Editor', function () {
   });
 
   it('should drag the first block and drop after the last block.', function () {
-    cy.get('[data-cy=editorjs]').get('div.ce-block').first().click();
-
-    const dataTransfer = new DataTransfer();
-    cy.get('.ce-toolbar__settings-btn').trigger('dragstart', { dataTransfer });
-
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
-      .eq(2)
-      .trigger('dragenter')
-      .then((blocks) => {
-        // Get the target block rect.
-        const targetBlockRect = blocks[0].getBoundingClientRect();
-        const yShiftFromMiddleLine = 20;
-        // Dragover on target block little bit below the middle line.
-        const dragOverYCoord =
-          targetBlockRect.y +
-          (targetBlockRect.height / 2 + yShiftFromMiddleLine);
-
-        cy.get('[data-cy=editorjs]')
-          .get('div.ce-block')
-          .eq(2)
-          .trigger('dragover', {
-            clientX: targetBlockRect.x,
-            clientY: dragOverYCoord,
-          })
-          .trigger('drop', { dataTransfer });
-      });
-
-    cy.get('.ce-toolbar__settings-btn').trigger('dragend', { dataTransfer });
-
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .then((blocks) => {
-        expect(blocks[0].textContent).to.eq('Block 1');
-        expect(blocks[1].textContent).to.eq('Block 2');
-        expect(blocks[2].textContent).to.eq('Block 0');
-      });
-  });
-
-  it('should drag the last block and drop before the first block.', function () {
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .eq(2)
+      .first()
       .click();
 
     const dataTransfer = new DataTransfer();
-    cy.get('.ce-toolbar__settings-btn').trigger('dragstart', { dataTransfer });
 
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .eq(0)
-      .trigger('dragenter')
-      .then((blocks) => {
-        // Get the target block rect.
-        const targetBlockRect = blocks[0].getBoundingClientRect();
-        const yShiftFromMiddleLine = -20;
-        // Dragover on target block little bit below the middle line.
-        const dragOverYCoord =
-          targetBlockRect.y +
-          (targetBlockRect.height / 2 + yShiftFromMiddleLine);
-
-        cy.get('[data-cy=editorjs]')
-          .get('div.ce-block')
-          .eq(0)
-          .trigger('dragover', {
-            clientX: targetBlockRect.x,
-            clientY: dragOverYCoord,
-          })
-          .trigger('drop', { dataTransfer });
-      });
-
-    cy.get('.ce-toolbar__settings-btn')
-      .trigger('dragend', { dataTransfer });
-
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .then((blocks) => {
-        expect(blocks[0].textContent).to.eq('Block 2');
-        expect(blocks[1].textContent).to.eq('Block 0');
-        expect(blocks[2].textContent).to.eq('Block 1');
-      });
-  });
-
-  it('should drag the first two block and drop after the last block.', function () {
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .eq(0)
-      .type('{selectall}')
-      .trigger('keydown', {
-        shiftKey: true,
-        keyCode: _.keyCodes.DOWN,
-      });
-
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .eq(1)
-      .trigger("mouseenter")
-      .trigger("mousemove")
-      .trigger("mouseleave");
-
-
-    const dataTransfer = new DataTransfer();
+    // eslint-disable-next-line cypress/require-data-selectors
     cy.get('.ce-toolbar__settings-btn')
       .trigger('dragstart', { dataTransfer });
 
@@ -267,36 +186,30 @@ describe('Drag and drop the block of Editor', function () {
           .trigger('drop', { dataTransfer });
       });
 
-    cy.get('.ce-toolbar__settings-btn').trigger('dragend', { dataTransfer });
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragend', { dataTransfer });
 
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
       .then((blocks) => {
-        expect(blocks[0].textContent).to.eq('Block 2');
-        expect(blocks[1].textContent).to.eq('Block 0');
-        expect(blocks[2].textContent).to.eq('Block 1');
+        expect(blocks[0].textContent).to.eq('Block 1');
+        expect(blocks[1].textContent).to.eq('Block 2');
+        expect(blocks[2].textContent).to.eq('Block 0');
       });
   });
 
-  it('should drag the last two block and drop before the first block.', function () {
-    cy.get('[data-cy=editorjs]')
-      .get('div.ce-block')
-      .eq(1)
-      .type('{selectall}')
-      .trigger('keydown', {
-        shiftKey: true,
-        keyCode: _.keyCodes.DOWN,
-      });
-
+  it('should drag the last block and drop before the first block.', function () {
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
       .eq(2)
-      .trigger("mouseenter")
-      .trigger("mousemove")
-      .trigger("mouseleave");
+      .click();
 
     const dataTransfer = new DataTransfer();
-    cy.get('.ce-toolbar__settings-btn').trigger('dragstart', { dataTransfer });
+
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragstart', { dataTransfer });
 
     cy.get('[data-cy=editorjs]')
       .get('div.ce-block')
@@ -321,6 +234,126 @@ describe('Drag and drop the block of Editor', function () {
           .trigger('drop', { dataTransfer });
       });
 
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragend', { dataTransfer });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .then((blocks) => {
+        expect(blocks[0].textContent).to.eq('Block 2');
+        expect(blocks[1].textContent).to.eq('Block 0');
+        expect(blocks[2].textContent).to.eq('Block 1');
+      });
+  });
+
+  it('should drag the first two block and drop after the last block.', function () {
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(0)
+      .type('{selectall}')
+      .trigger('keydown', {
+        shiftKey: true,
+        keyCode: _.keyCodes.DOWN,
+      });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(1)
+      .trigger('mouseenter')
+      .trigger('mousemove')
+      .trigger('mouseleave');
+
+
+    const dataTransfer = new DataTransfer();
+
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragstart', { dataTransfer });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(2)
+      .trigger('dragenter')
+      .then((blocks) => {
+        // Get the target block rect.
+        const targetBlockRect = blocks[0].getBoundingClientRect();
+        const yShiftFromMiddleLine = 20;
+        // Dragover on target block little bit below the middle line.
+        const dragOverYCoord =
+          targetBlockRect.y +
+          (targetBlockRect.height / 2 + yShiftFromMiddleLine);
+
+        cy.get('[data-cy=editorjs]')
+          .get('div.ce-block')
+          .eq(2)
+          .trigger('dragover', {
+            clientX: targetBlockRect.x,
+            clientY: dragOverYCoord,
+          })
+          .trigger('drop', { dataTransfer });
+      });
+
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragend', { dataTransfer });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .then((blocks) => {
+        expect(blocks[0].textContent).to.eq('Block 2');
+        expect(blocks[1].textContent).to.eq('Block 0');
+        expect(blocks[2].textContent).to.eq('Block 1');
+      });
+  });
+
+  it('should drag the last two block and drop before the first block.', function () {
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(1)
+      .type('{selectall}')
+      .trigger('keydown', {
+        shiftKey: true,
+        keyCode: _.keyCodes.DOWN,
+      });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(2)
+      .trigger('mouseenter')
+      .trigger('mousemove')
+      .trigger('mouseleave');
+
+    const dataTransfer = new DataTransfer();
+
+    // eslint-disable-next-line cypress/require-data-selectors
+    cy.get('.ce-toolbar__settings-btn')
+      .trigger('dragstart', { dataTransfer });
+
+    cy.get('[data-cy=editorjs]')
+      .get('div.ce-block')
+      .eq(0)
+      .trigger('dragenter')
+      .then((blocks) => {
+        // Get the target block rect.
+        const targetBlockRect = blocks[0].getBoundingClientRect();
+        const yShiftFromMiddleLine = -20;
+        // Dragover on target block little bit below the middle line.
+        const dragOverYCoord =
+          targetBlockRect.y +
+          (targetBlockRect.height / 2 + yShiftFromMiddleLine);
+
+        cy.get('[data-cy=editorjs]')
+          .get('div.ce-block')
+          .eq(0)
+          .trigger('dragover', {
+            clientX: targetBlockRect.x,
+            clientY: dragOverYCoord,
+          })
+          .trigger('drop', { dataTransfer });
+      });
+
+    // eslint-disable-next-line cypress/require-data-selectors
     cy.get('.ce-toolbar__settings-btn')
       .trigger('dragend', { dataTransfer });
 
