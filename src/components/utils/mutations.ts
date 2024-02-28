439adcd410c67ae7ad351c796f4bd011bcb5ff66
Kanstantsin_Vikhor <konstantin.vikhor@gmail.com>
2024-02-28 11:02:47 +0400

    isMutationBelongsToElement function shouldn't return true if some of the ancestors of the passed element were added or deleted, only if the element itself

--- src/components/utils/mutations.ts
+++ src/components/utils/mutations.ts
@@ -12,11 +12,11 @@ export function isMutationBelongsToElement(mutationRecord: MutationRecord, eleme
   }
 
   if (type === 'childList') {
-    const elementAddedItself = Array.from(addedNodes).some(node => node.contains(element));
+    const elementAddedItself = Array.from(addedNodes).some(node => node === element);
     if (elementAddedItself) {
       return true;
     }
-    const elementRemovedItself = Array.from(removedNodes).some(node => node.contains(element));
+    const elementRemovedItself = Array.from(removedNodes).some(node => node === element);
     if (elementRemovedItself) {
       return true;
     }
