16fd53151be2d6c31f6ebaf2cf61543cad255c41
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2024-05-04 17:57:14 +0300

    Update jsdoc

--- src/components/utils/popover/components/popover-item/popover-item.types.ts
+++ src/components/utils/popover/components/popover-item/popover-item.types.ts
@@ -2,10 +2,10 @@
  * Popover item types
  */
 export enum PopoverItemType {
-  /** Default popover item */
+  /** Regular item with icon, title and other properties */
   Default = 'default',
 
-  /** Separator item*/
+  /** Gray line used to separate items from each other */
   Separator = 'separator',
 
   /** Item with custom html content */
