91959bba4397a024449955c377809ae6b7819196
Peter <specc.dev@gmail.com>
2024-07-10 19:16:36 +0300

    fix(on-change): onchange callback wont be fired in readonly (#2773)
    * fix(on-change): onchange callback wont be fired in readonly
    
    * do not rerender blocks on initial call

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -3,3 +3,9 @@
  * {@link modules/ui.ts}
  */
 export const selectionChangeDebounceTimeout = 180;
+
+/**
+ * Timeout for batching of DOM changes used by the ModificationObserver
+ * {@link modules/modificationsObserver.ts}
+ */
+export const modificationsObserverBatchTimeout = 400;
--- src/components/modules/modificationsObserver.ts
+++ src/components/modules/modificationsObserver.ts
@@ -2,6 +2,7 @@ import { BlockId } from '../../../types';
 import { BlockMutationEvent, BlockMutationType } from '../../../types/events/block';
 import { ModuleConfig } from '../../types-internal/module-config';
 import Module from '../__module';
+import { modificationsObserverBatchTimeout } from '../constants';
 import { BlockChanged, FakeCursorAboutToBeToggled, FakeCursorHaveBeenSet, RedactorDomChanged } from '../events';
 import * as _ from '../utils';
 
@@ -39,7 +40,7 @@ export default class ModificationsObserver extends Module {
   /**
    * Fired onChange events will be batched by this time
    */
-  private readonly batchTime = 400;
+  private readonly batchTime = modificationsObserverBatchTimeout;
 
   /**
    * Prepare the module
