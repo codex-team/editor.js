3cd24c4da17d21acd04ef1df95e17d2db93790c3
e11sy <130844513+e11sy@users.noreply.github.com>
2024-10-16 21:57:43 +0300

    Chore (toolbox): improved shortcuts visibility when tool exports array of toolbox items (#2846)
    * toolbox items logic improved
    
    * typo
    
    * lint fix
    
    * logic improved
    
    * make displaySecondaryLabel true by default
    
    * eslint fix
    
    * added testcase
    
    * updated changelog
    
    * typo
    
    * lint fix

--- test/cypress/tests/ui/toolbox.cy.ts
+++ test/cypress/tests/ui/toolbox.cy.ts
@@ -114,5 +114,105 @@ describe('Toolbox', function () {
           expect(blocks[1].type).to.eq('nonConvertableTool');
         });
     });
+
+    it('should display shortcut only for the first toolbox item if tool exports toolbox with several items', function () {
+      /**
+       * Mock of Tool with conversionConfig
+       */
+      class ToolWithSeveralToolboxItems extends ToolMock {
+        /**
+         * Specify toolbox with several items related to one tool
+         */
+        public static get toolbox(): ToolboxConfig {
+          return [
+            {
+              icon: '',
+              title: 'first tool',
+            },
+            {
+              icon: '',
+              title: 'second tool',
+            },
+          ];
+        }
+      }
+
+      cy.createEditor({
+        tools: {
+          severalToolboxItemsTool: {
+            class: ToolWithSeveralToolboxItems,
+            shortcut: 'CMD+SHIFT+L',
+          },
+        },
+      });
+
+      cy.get('[data-cy=editorjs]')
+        .find('.ce-paragraph')
+        .click()
+        .type('Some text')
+        .type('/'); // call a shortcut for toolbox
+
+      /**
+       * Secondary title (shortcut) should exist for first toolbox item of the tool
+       */
+      /* eslint-disable-next-line cypress/require-data-selectors */
+      cy.get('.ce-popover')
+        .find('.ce-popover-item[data-item-name="severalToolboxItemsTool"]')
+        .first()
+        .find('.ce-popover-item__secondary-title')
+        .should('exist');
+
+      /**
+       * Secondary title (shortcut) should not exist for second toolbox item of the same tool
+       */
+      /* eslint-disable-next-line cypress/require-data-selectors */
+      cy.get('.ce-popover')
+        .find('.ce-popover-item[data-item-name="severalToolboxItemsTool"]')
+        .eq(1)
+        .find('.ce-popover-item__secondary-title')
+        .should('not.exist');
+    });
+
+    it('should display shortcut for the item if tool exports toolbox as an one item object', function () {
+      /**
+       * Mock of Tool with conversionConfig
+       */
+      class ToolWithOneToolboxItems extends ToolMock {
+        /**
+         * Specify toolbox with several items related to one tool
+         */
+        public static get toolbox(): ToolboxConfig {
+          return {
+            icon: '',
+            title: 'tool',
+          };
+        }
+      }
+
+      cy.createEditor({
+        tools: {
+          oneToolboxItemTool: {
+            class: ToolWithOneToolboxItems,
+            shortcut: 'CMD+SHIFT+L',
+          },
+        },
+      });
+
+      cy.get('[data-cy=editorjs]')
+        .find('.ce-paragraph')
+        .click()
+        .type('Some text')
+        .type('/'); // call a shortcut for toolbox
+
+      /**
+       * Secondary title (shortcut) should exist for toolbox item of the tool
+       */
+      /* eslint-disable-next-line cypress/require-data-selectors */
+      cy.get('.ce-popover')
+        .find('.ce-popover-item[data-item-name="oneToolboxItemTool"]')
+        .first()
+        .find('.ce-popover-item__secondary-title')
+        .should('exist');
+    });
   });
 });
