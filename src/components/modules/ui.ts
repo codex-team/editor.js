582e137b77a159aa7d9d6c6459e9694d426463ad
Eugeny <akulistus555@gmail.com>
2026-03-04 02:23:23 +0300

    fix test errors

--- src/components/modules/ui.ts
+++ src/components/modules/ui.ts
@@ -771,10 +771,11 @@ export default class UI extends Module<UINodes> {
      * case when user clicks on anchor element
      * if it is clicked via ctrl key, then we open new window with url
      */
-    const element = (event.target as Element).closest("a");
+    const element = event.target as Element;
     const ctrlKey = event.metaKey || event.ctrlKey;
+    const hasParentAnchor = !!(event.target as Element).closest("a");
 
-    if ($.isAnchor(element) && ctrlKey) {
+    if ((hasParentAnchor || $.isAnchor(element)) && ctrlKey) {
       event.stopImmediatePropagation();
       event.stopPropagation();
 
